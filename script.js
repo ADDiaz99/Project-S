c2d7d887438e6dc685a4b42af44b555ed057be19
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2024-04-29 21:02:24 -0500

    added script functionality, centered divs

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function proceso() {
-    let nota1 = parseFloat(document.getElementById("nota1").value);
-    let nota2 = parseFloat(document.getElementById("nota2").value);
-    let result = nota1 + nota2;
-    document.getElementById("result").innerHTML = "Result: " + result;
+    let nota_1 = parseFloat(document.getElementById("nota_1").value);
+    let nota_2 = parseFloat(document.getElementById("nota_2").value);
+    let result = 0;
+    
+    for(let nota_3 = 0; nota_3 < 8; nota_3 += 0.1){
+        if(nota_1 * 0.3 + nota_2 * 0.3 + nota_3 * 0.4 >= 3){
+            result = nota_3;
+            break;
+        }
+    }
+    document.getElementById("result").innerHTML = "Para pasar necesitas: " + result.toFixed(1) + "\nMucha suerte! ";
 }
